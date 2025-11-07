7db7d67449ee1c18bcf0b6a0a6167ac2e70965f9
sumitdabhi002 <sumitdabhi002@gmail.com>
2025-11-07 15:53:23 +0530

    database configuration

--- firstOne/My/urls.py
+++ firstOne/My/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.all_my,name='all_home'),
+    path('<int:chai_id>/',views.chai_detail,name='chai_details'),
 ]
--- firstOne/My/views.py
+++ firstOne/My/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import ChaiVarity
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
-def all_my(reqest):
-  return render(reqest,'My/all_My.html')
+def all_my(request):
+    chais = ChaiVarity.objects.all()
+    return render(request, 'My/all_My.html', {'chais': chais})
+
+def chai_detail(request,chai_id):
+    chai = get_object_or_404(ChaiVarity,pk=chai_id)
+    return render(request,'my/chai_details.html',{'chai':chai})
